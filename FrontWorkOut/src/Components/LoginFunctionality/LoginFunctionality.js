2c70557cc99a9687baeb9341bd7d2d7feb78153f
Cornici Vlad <49037796+vladik9@users.noreply.github.com>
2023-08-17 15:01:51 +0300

    Revert "added react router, and connected get exercise api to back end"
    This reverts commit 3d282c417cf9edf15a31238b99b13d922f434e33.

--- FrontWorkOut/src/Components/LoginFunctionality/LoginFunctionality.js
+++ FrontWorkOut/src/Components/LoginFunctionality/LoginFunctionality.js
@@ -2,11 +2,10 @@ import React from "react";
 import SignIn from "./SingIn/SignIn";
 import SignUp from "./SignUp/SignUp";
 import LoginContext from "./LoginContext/LoginContext";
-import { Route, Routes } from "react-router-dom";
 export default function LoginFunctionality() {
   return (
     <LoginContext>
-      <SignIn />
+      <SignUp />
     </LoginContext>
   );
 }
