98f636c89df19a9a41aba5f8af2cb4599055b7f6
Cornici Vlad <49037796+vladik9@users.noreply.github.com>
2023-08-17 15:46:16 +0300

    Revert "added react router, and connected get exercise api to back end"
    This reverts commit 3d282c417cf9edf15a31238b99b13d922f434e33.

--- BackWorkOut/routers/exercise.js
+++ BackWorkOut/routers/exercise.js
@@ -2,16 +2,44 @@ const express = require("express");
 const Eexercise = require("../models/exercise");
 const auth = require("../middleware/auth");
 const router = new express.Router();
+// ######################
+// ####Data base access##
+//token key
+//get body parts for next request
+router.get("/getBodyParts", async (req, res) => {
+  const bodyParts = [
+    "back",
+    "cardio",
+    "chest",
+    "lower arms",
+    "lower legs",
+    "neck",
+    "shoulders",
+    "upper arms",
+    "upper legs",
+    "waist",
+  ];
+  res.send(bodyParts);
+});
+//this endpoint should get paggination
+router.get("/getExerciseByBodyPart/:id", async (req, res) => {
+  let response;
+  const options = {
+    method: "GET",
+    url: `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${req.params.id}`,
+    headers: {
+      "X-RapidAPI-Key": "8e9fc8d0afmshcc4af32a9be6604p1a8318jsndee4724a6a76",
+      "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    },
+  };
 
-//when user wants to gett all exercuses
-
-router.post("/saveNewExercise", ath, async (req, res) => {
-  //cretating a new exercise record
-  const exercise = new Exercise({ ...req.body, owner: req.user._id });
   try {
-    await exercise.save();
+    response = await axios.request(options);
+    console.log(response);
+    res.send(response.data);
   } catch (error) {
-    console.log("Error in saving an exercise" + error);
+    console.error("Error in getting exercise data", error);
+    res.send(500, error);
   }
 });
 
