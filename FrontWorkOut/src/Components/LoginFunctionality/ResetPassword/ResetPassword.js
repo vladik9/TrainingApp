eae1b1cdbf149b167d6f9e8cf790d813a2968db6
Vlad Cornici <v.cornici@gmail.com>
2023-09-09 10:10:38 +0300

    Removed unesed dependecies on ResetParsword Component

--- FrontWorkOut/src/Components/LoginFunctionality/ResetPassword/ResetPassword.js
+++ FrontWorkOut/src/Components/LoginFunctionality/ResetPassword/ResetPassword.js
@@ -1,15 +1,12 @@
 import React, { useContext } from "react";
-import Avatar from "@mui/material/Avatar";
+
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Copyright from "../Copyright/Copyright.js";
 import LoginContext from "../LoginContext/LoginContext.js";
